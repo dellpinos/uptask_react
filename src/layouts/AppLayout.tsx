15cf4ea3a2d126ce0cfdf164fd5335d6331d9cd9
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-12-17 11:14:12 -0300

    routing

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+function AppLayout() {
+  return (
+    <>
+        <div>AppLayout</div>
+
+        <Outlet />
+    </>
+  )
+}
+
+export default AppLayout
