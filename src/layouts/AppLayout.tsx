4b21ab6ed20a3086789a77be17d3927e2069008c
Martin del Pino <dellpinos7@gmail.com>
2024-12-19 19:54:38 -0300

    adding react hook form

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
 function AppLayout() {
   return (
     <>
@@ -10,7 +11,10 @@ function AppLayout() {
                 <div className=" w-80">
                     < Logo />
                 </div>
+
+                <NavMenu />
             </div>
+
         </header>
         <section className="max-w-screen-2xl mx-auto mt-10 p-5">
             <Outlet />
