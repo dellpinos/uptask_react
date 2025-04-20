b229263ad14b67dd7af2d62d45dc28e7a58e9337
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2025-04-20 18:13:40 -0300

    trabajando en auth

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { ConfirmToken, ForgotPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
+import type { ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 
 
 export async function createAccount(formData : UserRegistrationForm) {
@@ -50,8 +50,9 @@ export async function authenticateUser(formData : UserLoginForm) {
     try {
         const url = '/auth/login';
         const { data } = await api.post<string>(url, formData);
-        return data;
 
+        localStorage.setItem('AUTH_TOKEN_TT3', data);
+        return data;
         
     } catch (error) {
         if(isAxiosError(error) && error.response) {
@@ -64,6 +65,36 @@ export async function forgotPassword(formData : ForgotPasswordForm) {
 
     try {
         const url = '/auth/forgot-password';
+        const { data } = await api.post<string>(url, formData);
+        return data;
+
+        
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function validateToken(formData : ConfirmToken) {
+
+    try {
+        const url = '/auth/validate-token';
+        const { data } = await api.post<string>(url, formData);
+        return data;
+
+        
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error);
+        }
+    }
+}
+
+export async function updatePasswordWithToken({formData, token} : {formData: NewPasswordForm, token: ConfirmToken['token']}) {
+
+    try {
+        const url = `/auth/update-password/${token}`;
         const { data } = await api.post<string>(url, formData);
         return data;
 
