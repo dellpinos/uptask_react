a07b650e146fd6c15ce6768bb7362a514420f52c
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2025-07-01 19:12:00 -0300

    listing teams members

---
+++ src/components/team/SearchResult.tsx
@@ -0,0 +1,50 @@
+import { addUserToProject } from "@/api/TeamAPI";
+import { TeamMember } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
+type SearchResultProps = {
+    user: TeamMember,
+    reset: () => void
+}
+
+export default function SearchResult({ user, reset }: SearchResultProps) {
+
+    const params = useParams();
+    const projectId = params.projectId!;
+    const navigate = useNavigate();
+
+    const { mutate } = useMutation({
+        mutationFn: addUserToProject,
+        onError: (error) => {
+            toast.error(error.message);
+        },
+        onSuccess: (data) => {
+            toast.success(data);
+            reset();
+            navigate(location.pathname, {replace: true});
+        }
+    });
+
+    const handleAddUserToProject = () => {
+        const data = {
+            projectId,
+            id : user._id
+        }
+        mutate(data);
+    }
+
+    return (
+        <>
+            <p className="mt-10 text-center font-bold">Resultado:</p>
+            <div className="flex justify-between items-center">
+                <p>{user.name}</p>
+                <button className="text-purple-600 hover:bg-purple-100 cursor-pointer px-10 py-3 font-bold"
+                onClick={handleAddUserToProject}>
+                    Agregar al Proyecto
+                </button>
+            </div>
+        </>
+    )
+}
